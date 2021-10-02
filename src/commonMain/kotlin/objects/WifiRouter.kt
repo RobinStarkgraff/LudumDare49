3c8adaf6c559663a1536658ee23afceef7391ff1
alineary <alinadvries@gmail.com>
2021-10-02 18:40:27 +0200

    Added absolute numbers as well as a more intuitive if/else

--- src/commonMain/kotlin/objects/WifiRouter.kt
+++ src/commonMain/kotlin/objects/WifiRouter.kt
@@ -4,19 +4,20 @@ import com.soywiz.korge.view.Circle
 import com.soywiz.korge.view.Image
 import com.soywiz.korge.view.xy
 import com.soywiz.korim.color.RGBA
+import kotlin.math.absoluteValue
 import kotlin.math.pow
 import kotlin.math.sqrt
 
 class WifiRouter(private val x: Double, private val y: Double, radius: Double, private val wifiRouterImg: Image) {
 
     companion object {
-       lateinit var wifiRouters: List<WifiRouter>
+        lateinit var wifiRouters: List<WifiRouter>
     }
 
     private val circle = Circle(radius, fill = RGBA(255, 44, 52, 150))
 
     fun drawImage() {
-        wifiRouterImg.xy(x, y);
+        wifiRouterImg.xy(x, y)
         circle.xy(x, y)
     }
 
@@ -24,17 +25,17 @@ class WifiRouter(private val x: Double, private val y: Double, radius: Double, p
 
         val section: Double = circle.radius / 4
 
-        val xDifference: Double = if (circle.x >= playerX) circle.x - playerX else playerX - circle.x;
-        val yDifference: Double = if (circle.y >= playerY) circle.y - playerY else playerY - circle.y;
+        val xDifference: Double = circle.x.absoluteValue - playerX.absoluteValue
+        val yDifference: Double = circle.y.absoluteValue - playerY.absoluteValue
 
         var hypotenuse = xDifference.pow(2) + yDifference.pow(2)
         hypotenuse = sqrt(hypotenuse)
 
-        return if (hypotenuse <= section) 4                                 // the innermost section
-        else if (hypotenuse <= section * 2 && hypotenuse > section) 3       // second inner section, hypotenuse must be longer than fits into the first section
-        else if (hypotenuse <= section * 3 && hypotenuse > section * 2) 2   // third inner section
-        else if (hypotenuse <= circle.radius && hypotenuse > section * 3) 1 // the outermost section
-        else 0                                                              // outside of the wifi radius
+        return if (hypotenuse >= circle.radius) 0   // outside the wifi signal
+        else if (hypotenuse >= section * 3) 1       // outermost section
+        else if (hypotenuse >= section * 2) 2       // second outer section
+        else if (hypotenuse >= section) 3           // the third section
+        else 4                                      // inside the fourth section
 
     }
 }
