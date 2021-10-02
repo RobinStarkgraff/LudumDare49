5fbfbeca1d75503d2a6a66cec2e35ff84c1b063c
eleanmai <eric.lean.maier93@gmail.com>
2021-10-02 12:55:30 +0200

    add player movement

--- src/commonMain/kotlin/player.kt
+++ src/commonMain/kotlin/player.kt
@@ -11,24 +11,25 @@ class Player(private val scene : Scene) {
     init {
         val image = scene.sceneView.solidRect(SIZE, SIZE)
         movement(image)
+
     }
 
     private fun movement(player: SolidRect){
-        val input = scene.views.input
-        player.addUpdater { dt ->
-            val scale = dt / 16.milliseconds
-            var movement = Vector2D(0, 0)
+            player.addUpdater { dt ->
+                val input = scene.views.input
+                val scale = dt / 16.milliseconds
+                var movement = Vector2D(0, 0)
 
-            if (input.keys.pressing(Key.LEFT) || input.keys.pressing(Key.A)) movement.x -= 2.0 * scale
-            if (input.keys.pressing(Key.RIGHT) || input.keys.pressing(Key.D)) movement.x += 2.0 * scale
-            if (input.keys.pressing(Key.UP) || input.keys.pressing(Key.W)) movement.y -= 2.0 * scale
-            if (input.keys.pressing(Key.DOWN) || input.keys.pressing(Key.S)) movement.y += 2.0 * scale
+                if (input.keys.pressing(Key.LEFT) || input.keys.pressing(Key.A)) movement.x -= 1.0
+                if (input.keys.pressing(Key.RIGHT) || input.keys.pressing(Key.D)) movement.x += 1.0
+                if (input.keys.pressing(Key.UP) || input.keys.pressing(Key.W)) movement.y -= 1.0
+                if (input.keys.pressing(Key.DOWN) || input.keys.pressing(Key.S)) movement.y += 1.0
 
-            if (movement.length > 0) {
-                movement.normalize()
-                movement *= SPEED
-                println(movement)
-                xy(x + movement.x, y + movement.y)
+                if (movement.length > 0) {
+                    movement.normalize()
+                    movement *= SPEED
+                    movement *= scale
+                    xy(x + movement.x, y + movement.y)
             }
         }
     }
