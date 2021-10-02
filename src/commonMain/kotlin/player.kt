fa2c4d09eae171bb7cb2732ef841b05e34a679a7
eleanmai <eric.lean.maier93@gmail.com>
2021-10-02 14:40:40 +0200

    Changes by Bennet

--- src/commonMain/kotlin/player.kt
+++ src/commonMain/kotlin/player.kt
@@ -7,7 +7,7 @@ import com.soywiz.korma.geom.Vector2D
 const val SPEED = 5
 const val SIZE = 100
 
-class Player(private val scene : Scene) {
+class Player(private val scene: Scene) {
     init {
 
         val image = scene.sceneView.solidRect(SIZE, SIZE).xy(200, 200)
@@ -21,16 +21,16 @@ class Player(private val scene : Scene) {
             val scale = dt / 16.milliseconds
             var movement = Vector2D(0, 0)
 
-                if (input.keys.pressing(Key.LEFT) || input.keys.pressing(Key.A)) movement.x -= 1.0
-                if (input.keys.pressing(Key.RIGHT) || input.keys.pressing(Key.D)) movement.x += 1.0
-                if (input.keys.pressing(Key.UP) || input.keys.pressing(Key.W)) movement.y -= 1.0
-                if (input.keys.pressing(Key.DOWN) || input.keys.pressing(Key.S)) movement.y += 1.0
+            if (input.keys.pressing(Key.LEFT) || input.keys.pressing(Key.A)) movement.x -= 1.0
+            if (input.keys.pressing(Key.RIGHT) || input.keys.pressing(Key.D)) movement.x += 1.0
+            if (input.keys.pressing(Key.UP) || input.keys.pressing(Key.W)) movement.y -= 1.0
+            if (input.keys.pressing(Key.DOWN) || input.keys.pressing(Key.S)) movement.y += 1.0
 
-                if (movement.length > 0) {
-                    movement.normalize()
-                    movement *= SPEED
-                    movement *= scale
-                    xy(x + movement.x, y + movement.y)
+            if (movement.length > 0) {
+                movement.normalize()
+                movement *= SPEED
+                movement *= scale
+                xy(x + movement.x, y + movement.y)
             }
 
             val collisionList = listOf(player)
