132b58c7462180405f04ea26d36134b15f1c3f0d
alineary <alinadvries@gmail.com>
2021-10-02 16:20:35 +0200

    Renaming and such

--- src/commonMain/kotlin/classes/WifiRouter.kt
+++ src/commonMain/kotlin/classes/WifiRouter.kt
@@ -5,13 +5,13 @@ import com.soywiz.korge.view.Image
 import com.soywiz.korge.view.xy
 import com.soywiz.korim.color.RGBA
 
-class WifiRadius(private val x: Double, private val y: Double, radius: Double, private val wifiRouterImg: Image) {
+class WifiRouter(private val x: Double, private val y: Double, radius: Double, private val wifiRouterImg: Image) {
 
     companion object {
-       lateinit var wifiRouters: List<WifiRadius>
+       lateinit var wifiRouters: List<WifiRouter>
     }
 
-    private val circle = Circle(radius, fill = RGBA(255, 44, 52, 1))
+    private val circle = Circle(radius, fill = RGBA(255, 44, 52, 150))
 
     fun drawImage() {
         wifiRouterImg.xy(x, y);
