d3e1c6eb2c2f51fca2bb0351e67a52e4c2fa6a41
Bennet Rupnow <bennet.rupnow@gym-meiendorf.de>
2021-10-02 16:08:24 +0200

    Update player.kt

--- src/commonMain/kotlin/player.kt
+++ src/commonMain/kotlin/player.kt
@@ -4,32 +4,34 @@ import com.soywiz.korev.Key
 import com.soywiz.korge.view.*
 import com.soywiz.korma.geom.Vector2D
 
-const val SPEED = 3
-const val SIZE = 50
-
 class Player(private val scene: Level) {
-    private var image : SolidRect = scene.sceneView.solidRect(SIZE, SIZE).xy(scene.spawnpoint.x, scene.spawnpoint.y);
+    companion object {
+        const val SPEED = 3
+        const val SIZE = 50
+    }
+
+    private var playerImage : SolidRect = scene.sceneView.solidRect(SIZE, SIZE).xy(scene.spawnpoint.x, scene.spawnpoint.y);
     init {
         movement()
         deathZoneCallback()
     }
 
     private fun deathZoneCallback() {
-        image.onCollision({scene.deathZoneList.contains(it)}) {
+        playerImage.onCollision({scene.deathZoneList.contains(it)}) {
             die()
         }
     }
 
-    public fun die() {
+    private fun die() {
         //reset all stuff
-        image.xy(scene.spawnpoint.x, scene.spawnpoint.y)
+        playerImage.xy(scene.spawnpoint.x, scene.spawnpoint.y)
     }
 
     private fun movement() {
         val input = scene.views.input
 
         var movement = Vector2D(0, 0)
-        image.addUpdater { dt ->
+        playerImage.addUpdater { dt ->
             movement = Vector2D(0, 0)
             val scale = dt / 16.milliseconds
             if (input.keys.pressing(Key.LEFT) || input.keys.pressing(Key.A)) movement.x -= 1.0
@@ -45,12 +47,12 @@ class Player(private val scene: Level) {
             }
         }
 
-        image.onCollision({scene.collisionList.contains(it)}) {
+        playerImage.onCollision({scene.collisionList.contains(it)}) {
             if (movement.x != 0.0) {
-                image.x -= movement.x
+                playerImage.x -= movement.x
             }
             if (movement.y != 0.0) {
-                image.y -= movement.y
+                playerImage.y -= movement.y
             }
         }
     }
