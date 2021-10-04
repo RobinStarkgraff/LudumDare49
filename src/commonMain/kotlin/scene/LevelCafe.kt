06b6d20ba082466792eadbf4c67d3bc6d2bd589b
alineary <alinadvries@gmail.com>
2021-10-04 23:48:14 +0200

    Add speechbubble

--- src/commonMain/kotlin/scene/LevelCafe.kt
+++ src/commonMain/kotlin/scene/LevelCafe.kt
@@ -6,6 +6,7 @@ import com.soywiz.korma.geom.Vector2D
 import helper.SoundPlayer
 import helper.SpriteLibrary
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import objects.Phone
 import objects.Player
@@ -21,6 +22,7 @@ class LevelCafe : Level() {
         phone = Phone(this@LevelCafe)
         SoundPlayer.playBackgroundMusic(SoundPlayer.BGM1, -0.28)
         drawImages()
+        GlobalScope.launch { playerDialog() }
     }
 
     override fun downloadComplete() {
@@ -29,6 +31,10 @@ class LevelCafe : Level() {
                 GlobalScope.launch { nextScene() }
             }
         }
+    }
+
+    suspend fun playerDialog() {
+        player?.say("I must have dropped my change somewhere. I have to pay to get the WiFi password!", 3)
     }
 
     override lateinit var objective: ObjectiveItem
