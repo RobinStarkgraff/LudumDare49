418b4ae8ee95afec7fa6027703fd62dabd57b74e
eleanmai <eric.lean.maier93@gmail.com>
2021-10-04 09:35:00 +0200

    Scaling fixed

--- src/commonMain/kotlin/helper/ImageLibrary.kt
+++ src/commonMain/kotlin/helper/ImageLibrary.kt
@@ -10,7 +10,20 @@ class ImageLibrary {
     companion object {
         private const val ART_FOLDER_PATH = "art/"
 
+
         const val BACKGROUND_IMAGE_FLAT = "background/frog_flat.png"
+        const val KITCHEN_WALLS = "background/kitchenWalls.png"
+        const val BATHROOM_WALLS = "background/bathroomWalls.png"
+        const val FOREGROUND_LIGHT_FLAT = "foreground/FrogFlat_lightOverlay.png"
+        const val BATHTUB = "furniture/bathtub.png"
+        const val BOOKSHELF = "furniture/bookshelf.png"
+        const val COUCH = "furniture/couch.png"
+        const val COUCHTABLE = "furniture/couchtable.png"
+        const val DESK = "furniture/desk.png"
+        const val KITCHEN_ASSEMBLY = "furniture/kitchen_assembly.png"
+        const val MIRROR = "furniture/mirror.png"
+        const val NIGHT_STAND = "furniture/night_stand"
+        const val FLOWER_POT = "objects/flower_pot.png"
 
         suspend fun loadImage(parent: Level, filePath: String, anchorX: Double, anchorY: Double): Image {
             return parent.sceneView.image(resourcesVfs[ART_FOLDER_PATH + filePath].readBitmap(), anchorX, anchorY)
--- src/commonMain/kotlin/scene/Level1.kt
+++ src/commonMain/kotlin/scene/Level1.kt
@@ -21,7 +21,17 @@ class Level1 : Level() {
 
     override suspend fun drawImages() {
         super.drawImages()
-        val backgroundImage = ImageLibrary.loadImage(this, ImageLibrary.BACKGROUND_IMAGE_FLAT, 0.5, 0.5)
+        val backgroundImage = ImageLibrary.loadImage(this, ImageLibrary.BACKGROUND_IMAGE_FLAT, 0.0, 0.0)
+        val light = ImageLibrary.loadImage(this,ImageLibrary.FOREGROUND_LIGHT_FLAT, 0.0,0.0)
+        val bathtub = ImageLibrary.loadImage(this, ImageLibrary.BATHTUB, 0.0, 0.0).xy(100,100)
+        val bookshelf = ImageLibrary.loadImage(this, ImageLibrary.BOOKSHELF, 0.0, 0.0)
+        val couch = ImageLibrary.loadImage(this, ImageLibrary.COUCH, 0.0,0.0)
+        val couchtable = ImageLibrary.loadImage(this, ImageLibrary.COUCHTABLE, 0.0, 0.0)
+        val desk = ImageLibrary.loadImage(this, ImageLibrary.DESK, 0.0, 0.0)
+        val kitchen_assembly = ImageLibrary.loadImage(this, ImageLibrary.KITCHEN_ASSEMBLY, 0.0, 0.0)
+        val mirror = ImageLibrary.loadImage(this, ImageLibrary.MIRROR, 0.0, 0.0)
+        val night_stand = ImageLibrary.loadImage(this, ImageLibrary.NIGHT_STAND, 0.0, 0.0)
+
 
     }
 
