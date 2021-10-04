c2e992b5bf143e042629cf513475b6bc5c558271
bolam123 <moritz@butenob.net>
2021-10-04 10:46:57 +0200

    Calling of Sound
    implemented that the sound is called, fixed that the state of the door is right from level start, fixed that sound is not played during init

--- src/commonMain/kotlin/objects/interactables/StateSwapItem.kt
+++ src/commonMain/kotlin/objects/interactables/StateSwapItem.kt
@@ -2,12 +2,17 @@ package objects.interactables
 
 import com.soywiz.korge.view.Sprite
 import com.soywiz.korge.view.SpriteAnimation
+import helper.SoundPlayer
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import scene.Level
 
 class StateSwapItem(
     scene: Level,
     private val sprite: Sprite,
     animation: SpriteAnimation,
+    private val soundone: String,
+    private val soundtwo: String,
     override val interactionDistance: Double = 100.0
 ) :
     Interactable(scene, sprite.pos) {
@@ -15,13 +20,23 @@ class StateSwapItem(
         private var initialState = false
     }
 
+    private var setupsoundone = false
+    private var setupsoundtwo = false
+
     init {
         sprite.playAnimation(animation)
+        interact()
         interact()
     }
 
     override fun interact() {
-        if (initialState) sprite.setFrame(0) else sprite.setFrame(1)
+        if (initialState){
+            sprite.setFrame(0)
+            if (setupsoundone) GlobalScope.launch { SoundPlayer.playSound(soundone) } else setupsoundone = true
+        } else {
+            sprite.setFrame(1)
+            if (setupsoundtwo) GlobalScope.launch {SoundPlayer.playSound(soundtwo)} else setupsoundtwo = true
+        }
         initialState = !initialState
     }
 }
